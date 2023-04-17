3ea6e95903f9804b6077620cadf97b2c6c987591
Akio1102 <cristiandaviddiazaza@gmail.com>
2023-04-16 21:37:13 -0500

    feat (pokedex) :sparkles: star features pokedex

--- pokedex/main.js
+++ pokedex/main.js
@@ -1,28 +1,61 @@
-const url = `https://pokeapi.co/api/v2/pokemon`
+const listaPokemon = document.getElementById("listaPokemon");
 
-document.addEventListener("DOMContentLoaded",()=>{
-  render();
-})
+const url = `https://pokeapi.co/api/v2/pokemon`;
 
+// document.addEventListener("DOMContentLoaded", () => {
+//   render();
+// });
 
-const getData = async (id) =>{
+const getData = async (id) => {
   try {
-    const data = await fetch(`${url}/${id}`)
-    const result = await data.json()
-    return result
+    const data = await fetch(`${url}/${id}`);
+    const result = await data.json();
+    return result;
   } catch (error) {
     console.log(error);
   }
-}
+};
 
+const data = [];
 
 async function render() {
+  for (let index = 1; index <= 10; index++) {
+    data.push(await getData(index));
+    mostrarPokemon(await getData(index));
+  }
+  console.log(data);
+  //return data;
+}
+render();
 
-    const data = [];
-    for (let index = 0; index < 20; index++) { 
-        data.push(await getData(1));
-    }
-
-    console.log(data);
-    //return data;
+function mostrarPokemon(a) {
+  const div = document.createElement("div");
+  div.classList.add("col-3");
+  div.innerHTML = `
+   <div class="card text-center fs-3">
+            <div class="card-header">
+              <div class="row">
+                <p class="col-10 text-reset">${a.name}</p>
+                <p class="col-2 text-reset">${a.id}</p>
+              </div>
+            </div>
+            <div class="card-body">
+              <img
+                src="../assets/PokemonDiamante.jpg"
+                class="img-fluid m-auto"
+                alt="..."
+              />
+              <div class="card-title row">
+                <p class="col-6">weight</p>
+                <p class="col-6">height</p>
+              </div>
+              <div class="card-text">
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">Tipos</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+  `;
+  listaPokemon.append(div);
 }
